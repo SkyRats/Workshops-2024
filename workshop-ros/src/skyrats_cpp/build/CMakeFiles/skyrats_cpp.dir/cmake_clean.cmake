3c4259ae32830713d8ad9e56f100225794e469c2
helena <helenamoyen@usp.br>
2023-07-24 19:43:35 -0300

    added ros workshop

---
+++ workshop-ros/src/skyrats_cpp/build/CMakeFiles/skyrats_cpp.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/skyrats_cpp.dir/src/example.cpp.o"
+  "devel/lib/skyrats_cpp/skyrats_cpp"
+  "devel/lib/skyrats_cpp/skyrats_cpp.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/skyrats_cpp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ workshop-ros/src/skyrats_cpp/build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/helena/workshop_ws/src/skyrats_cpp
+# Build directory: /home/helena/workshop_ws/src/skyrats_cpp/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
